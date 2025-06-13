46bb9c78e8feb6f0f36b1d4fc87c5fc2ef8b7801
Onoda <onoda.k.829@ms.saitama-u.ac.jp>
2025-06-13 16:32:16 +0900

    Initial commit

---
+++ src/MyPackage.jl
@@ -0,0 +1,27 @@
+module MyPackage
+
+function perform_MyPackage(F::Function, tmax::Float64, x0::Float64, a0::Float64, h::Float64)::Tuple{Float64, Float64}
+    if tmax <= 0.0
+        error("tmax must be positive")
+    end
+    if h <= 0.0
+        error("h must be positive")
+    end
+
+    x::Float64 = x0
+    a::Float64 = a0
+    t::Float64 = 0.0
+    while t < tmax
+        a = a + h * F(x, t)
+        x = x + h * a
+        t = t + h
+    end
+
+    # デバッグ出力を追加
+    println("Debug: x_final = ", x)
+    println("Debug: a_final = ", a)
+
+    return (x, a)
+end
+
+end
